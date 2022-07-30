5eb8abb2f7c0f6887811f0eefce43149d733a32c
JulioTavaresDE <108236055+JulioTavaresDE@users.noreply.github.com>
2022-07-30 15:12:56 +0200

    Refatorando o formulário

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -8,9 +8,7 @@
         <div class="column">
                <div class="is-flex is-align-items-center is-justify-content-space-between"
                >
-                    <section>
-                        <strong>{{ tempoDecorrido }}</strong>
-                    </section>
+                    <Cronometro :tempo-em-segundos="tempoEmSegundos"></Cronometro>    
                     <button class="button" @click="iniciar">
                         <span class="icon">
                             <i class="fas fa-play"></i>
@@ -32,30 +30,29 @@
 
 <script lang="ts">
     import { defineComponent } from 'vue'
+    import Cronometro from './cronometro.vue';
     export default defineComponent({
-       name: 'MeuFormulario',
-       data(){
-            return {
-                tempoEmSegundos: 0
-            }
-       },
-
-        computed: {
-            tempoDecorrido(): string {
-                return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
-            }
+    name: "MeuFormulario",
+    components:{
+        Cronometro
+    },
+    data() {
+        return {
+            tempoEmSegundos: 0,
+            cronometro: 0
+        };
+    },
+    methods: {
+        iniciar() {
+            this.cronometro = setInterval(() => {
+                this.tempoEmSegundos += 1;
+            }, 1000);
+            console.log("iniciando");
         },
-
-       methods: {
-        iniciar(){
-            setInterval (()=>{
-                this.tempoEmSegundos+=1;
-            },1000)
-            console.log('iniciando');
-        },
-        finalizar(){
-            console.log('finalizando')
+        finalizar() {
+            clearInterval(this.cronometro);
         }
-       }
-    })
+    },
+    //components: { Cronometro }
+})
 </script>
