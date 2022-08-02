46cbce8ade464f2cca16e8e84bd5f8d53610383b
JulioTavaresDE <108236055+JulioTavaresDE@users.noreply.github.com>
2022-08-02 10:05:16 +0200

    Setup da store

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -31,25 +31,30 @@
 </template>
 
 <script lang="ts">
+import { useStore } from "@/store";
+import { computed } from "@vue/reactivity";
 import { defineComponent } from "vue";
-import IProjeto from '../interfaces/IProjeto'
+ 
 
 export default defineComponent ({
     name:'MeusProjetos',
     data() {
         return {
-            nomeDoProjeto: "",
-            projetos: [] as IProjeto[] 
+            nomeDoProjeto: ""
+             
         };
     },
     methods: {
         salvar () {
-            const projeto: IProjeto = {
-                nome : this.nomeDoProjeto,
-                id: new Date().toISOString()
-            }
-            this.projetos.push(projeto)
-            this.nomeDoProjeto = ' '
+            this.store.commit('ADICIONA_PROJETO',this.nomeDoProjeto)
+            this.nomeDoProjeto = "";
+        },
+    },
+    setup(){
+        const store = useStore()
+        return {
+            store,
+            projetos:computed(()=>store.state.projetos)
         }
     }
 })
