50ed84c26af325124951a699da9a71e026d35c69
JulioTavaresDE <108236055+JulioTavaresDE@users.noreply.github.com>
2022-08-03 12:12:41 +0200

    Notificando via store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,38 @@
+import { INotificacao, TipoNotificacao } from './../interfaces/INotificacao';
 import IProjeto from "@/interfaces/IProjeto";
 import { InjectionKey } from "vue";
 import { createStore,Store,useStore as vuexUseStore } from "vuex";
 import { ADICIONA_PROJETO, ALTERA_PROJETO, EXCLUIR_PROJETO } from "./tipo-mutacoes";
 
 interface Estado {
-    projetos: IProjeto[]
+    projetos: IProjeto[],
+    notificacoes: INotificacao[]
 }
 
 export const key: InjectionKey<Store<Estado>> = Symbol()
 export const store = createStore<Estado>({
     state: {
-        projetos : []
+        projetos : [],
+        notificacoes : [
+            {
+                id:1,
+                texto:'uma notificacao de sucesso',
+                titulo:'Sucesso',
+                tipo: TipoNotificacao.SUCESSO
+            },
+            {
+                id:2,
+                texto:'uma notificacao de atencao',
+                titulo:'Atencao',
+                tipo: TipoNotificacao.ATENCAO
+            },
+            {
+                id:3,
+                texto:'uma notificacao de falha',
+                titulo:'Falha',
+                tipo: TipoNotificacao.FALHA
+            }
+    ]
     },
     mutations: {
             [ADICIONA_PROJETO](state, nomeDoProjeto:string) {
