bd33a2ee09b1ab99d902977d96c932acf6f5a61c
JulioTavaresDE <108236055+JulioTavaresDE@users.noreply.github.com>
2022-08-09 11:54:49 +0200

    Emitindo eventos

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,79 +1,73 @@
 <template class="formulario">
-<div class="box formulario">
-    <div class="columns formulario">
-        <div class="column is-5" role="form" aria-label="Formulario para criacao de uma nova tarefa">
-            <input type="text" class="input" placeholder="Qual tarefa voce deseja iniciar?" v-model="descricao">
-        </div>
+    <div class="box formulario">
+        <div class="columns formulario">
+            <div class="column is-5" role="form" aria-label="Formulario para criacao de uma nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa voce deseja iniciar?" v-model="descricao">
+            </div>
 
-        <div class="column is-3">
-            <div class="select">
-                <select v-model="idProjeto">
-                    <option value="">Selecione o projeto</option>
-                    <option
-                        :value="projeto.id"
-                        v-for="projeto in projetos"
-                        :key="projeto.id"
-                    >
-                    {{ projeto.nome}}
-                    </option>
+            <div class="column is-3">
+                <div class="select">
+                    <select v-model="idProjeto">
+                        <option value="">Selecione o projeto</option>
+                        <option :value="projeto.id" v-for="projeto in projetos" :key="projeto.id">
+                            {{ projeto.nome }}
+                        </option>
 
-                </select>
+                    </select>
+                </div>
+            </div>
+
+
+            <div class="column">
+                <temporizador @aoTemporizadorFinalizado="finalizarTarefa"></temporizador>
             </div>
         </div>
-
-
-        <div class="column">
-            <temporizador @aoTemporizadorFinalizado="finalizarTarefa"></temporizador>
-        </div>
     </div>
-</div>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue'
-    import Temporizador from './Temporizador.vue';
-    import { useStore } from 'vuex'
-    import { key } from '@/store'
-    import { computed } from '@vue/reactivity';
-    
-    export default defineComponent({
+import { defineComponent, ref } from 'vue'
+import Temporizador from './Temporizador.vue';
+import { useStore } from 'vuex'
+import { key } from '@/store'
+import { computed } from '@vue/reactivity';
+
+export default defineComponent({
     name: "MeuTemporizador",
     emits: ['aoSalvarTarefa'],
-    components:{
-        Temporizador
+    components: {
+        Temporizador,
     },
 
-    data (){
-        return {
-            descricao: '',
-            idProjeto : ''
-        }
-    },
-
-    methods: {
-        finalizarTarefa(tempoDecorrido:number):void {
-            this.$emit('aoSalvarTarefa', {
-                 duracaoEmSegundos : tempoDecorrido,
-                 descricao: this.descricao,
-                 projeto: this.projetos.find(proj => proj.id == this.idProjeto)
-            })
-
-            console.log('Descricao da Tarefa', this.descricao)
-            this.descricao = ' '
-        }   
-    },
-    setup(){
+    setup(props, { emit}) {
         const store = useStore(key)
+
+        const descricao = ref("")
+        const idProjeto = ref("")
+        const projetos = computed(() => store.state.projeto.projetos)
+
+        const finalizarTarefa = (tempoDecorrido: number): void => {
+            emit('aoSalvarTarefa', {
+            duracaoEmSegundos: tempoDecorrido,
+            descricao: descricao.value,
+            projeto: projetos.value.find(proj => proj.id == idProjeto.value)
+            })
+            descricao.value = ' '
+        }
+
         return {
-            projetos: computed(()=> store.state.projeto.projetos)
+            descricao,
+            idProjeto,
+            projetos,
+            finalizarTarefa
         }
     }
-    });    
+});    
 </script>
 
 <style>
-    .formulario {
-        color: var(--texto-primario);
-        background-color: var(--bg-primario);
-    }
+.formulario {
+    color: var(--texto-primario);
+    background-color: var(--bg-primario);
+}
 </style>
