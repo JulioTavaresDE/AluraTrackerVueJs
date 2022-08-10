9a9b7d384bee224284520b08fc122114b728ced3
JulioTavaresDE <108236055+JulioTavaresDE@users.noreply.github.com>
2022-08-10 11:02:02 +0200

    Componentizando a modal

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -8,18 +8,36 @@
         <span class="icon is-small is-left">
           <i class="fas fa-search"></i>
         </span>
-        
       </p>
     </div>
 
     <tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" @aoTarefaClicada="selecionarTarefa">
     </tarefa>
+
+    <Modal :mostrar="tarefaSelecionada !=null">
+       <header class="modal-card-head">
+          <p class="modal-card-title">Editando uma tarefa</p>
+          <button @click="fecharModal" class="delete" aria-label="close"></button>
+        </header>
+        <section class="modal-card-body">
+          <div class="field">
+            <label for="descricaoDaTarefa" class="label">Descricao</label>
+            <input type="text" class="input" v-model="tarefaSelecionada.descricao" id="descricaoDaTarefa">
+          </div>
+        </section>
+        <footer class="modal-card-foot">
+          <button @click="alterarTarefa" class="button is-success">Salvar alteracoes</button>
+          <button @click="fecharModal" class="button">Cancelar</button>
+        </footer>
+    </Modal>
     
     <Box v-if="listaEstaVazia">
       Voce nao esta produtivo hoje :(
     </Box>
+
     
-    <div class="modal" :class="{ 'is-active': tarefaSelecionada }" v-if="tarefaSelecionada">
+
+    <!-- <div class="modal" :class="{ 'is-active': tarefaSelecionada }" v-if="tarefaSelecionada">
       <div class="modal-background"></div>
       <div class="modal-card">
         <header class="modal-card-head">
@@ -37,7 +55,7 @@
           <button @click="fecharModal" class="button">Cancelar</button>
         </footer>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -52,14 +70,16 @@ import { CADASTRAR_TAREFA } from '@/store/tipo-acoes';
 import ITarefa from '@/interfaces/ITarefa';
 import { OBTER_PROJETOS } from '@/store/tipo-acoes';
 import { ALTERAR_TAREFA } from '@/store/tipo-acoes';
+import Modal from "../components/Modal.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    Formulario,
+    Formulario, 
     Tarefa,
-    Box
-  },
+    Box,
+    Modal
+},
   data() {
     return {
       tarefaSelecionada: null as ITarefa | null
